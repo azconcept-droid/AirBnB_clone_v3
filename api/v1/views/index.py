ca2215a1deeaf67330c29da3be41084cd4ae9259
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-25 17:16:09 +0100

    add /stats endpoint that get obj count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,17 @@
 """
 from flask import jsonify
 from models import storage
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from api.v1.views import app_views
+
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -18,4 +27,9 @@ def get_status():
 def get_stats():
     """ get objects counts """
 
-    return jsonify(storage.count())
+    obj_count = {}
+
+    for cls in classes:
+        obj_count[cls] = storage.count(classes[cls])
+
+    return jsonify(obj_count)
