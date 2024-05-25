d2dfb5eadd3240df1a9393b4b3f1c89fcf1dadad
Yahaya Azeez <azconcept2016@gmail.com>
2024-05-25 16:15:54 +0100

    ignore pep8 and add /stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,12 +2,20 @@
 """ Index view
 """
 from flask import jsonify
+from models import storage
 
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
+def get_status():
     """ Status of the web server
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """ get objects counts """
+
+    return jsonify(storage.count())
